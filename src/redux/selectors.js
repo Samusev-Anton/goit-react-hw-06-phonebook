5867119a85d8546ab7b2d339575c81acd33dfc04
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-01-03 12:19:15 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
-export const isLoggedIn = state => state.user.isLoggedIn;
-const contacts = useSelector(state => state.contacts);
+// export const isLoggedIn = state => state.user.isLoggedIn;
+// // const contacts = useSelector(state => state.contacts);
